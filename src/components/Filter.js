74a561d94332429f7f8b43514b31bd8aa61b1be9
SotMDev <m.erkan.ozturk@gmail.com>
2022-11-26 10:27:14 +0300

    Filter component minor fixes

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import style from '../assets/styles/components/filters.scss'
 import API from "../services/API";
 
@@ -9,15 +9,21 @@ const Filter = ({characterData, setCharacterData}) => {
 
 	const filterProducts = (type) => {
 		setSearchParam(type);
-		Service.Request(`${process.env.REACT_APP_CHARACTERS_API}?status=${type}`).then(characters => setCharacterData(characters));
 	}
+
+	useEffect(() => {
+		if(searchParam !== ''){
+			Service.Request(`${process.env.REACT_APP_CHARACTERS_API}?status=${searchParam}`).then(characters => setCharacterData(characters));
+		}
+	}, [searchParam]);
+
 	return (
 		<div className={"filter"}>
 			<h3>Filter by status: </h3>
 			<div className={"filters"}>
-				<div className="button dead" onClick={() => filterProducts('dead')}>Dead</div>
-				<div className="button alive" onClick={() => filterProducts('alive')}>Alive</div>
-				<div className="button unknown" onClick={() => filterProducts('unknown')}>Unknown</div>
+				<div className={`button dead ${searchParam === 'dead' ? 'active' : ''}`} onClick={() => filterProducts('dead')}>Dead</div>
+				<div className={`button alive ${searchParam === 'alive' ? 'active' : ''}`} onClick={() => filterProducts('alive')}>Alive</div>
+				<div className={`button unknown ${searchParam === 'unknown' ? 'active' : ''}`} onClick={() => filterProducts('unknown')}>Unknown</div>
 			</div>
 		</div>
 	)
